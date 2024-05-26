9cad6ec7d345e2353f58084b353e3c70e772e048
ThePlano <162068101+ThePlano@users.noreply.github.com>
2024-05-26 20:33:39 -0300

    Add files via upload

---
+++ Curso.sql
@@ -0,0 +1,10 @@
+INSERT INTO Curso (nome, matriz_curricular)
+VALUES 
+('Ciência da Computação - Diurno', 1),
+('Ciência da Computação - Noturno', 2),
+('Engenharia Elétrica - Diurno', 1),
+('Engenharia Elétrica - Noturno', 2),
+('Matemática - Diurno', 1),
+('Matemática - Noturno', 2),
+('Administração - Diurno', 1),
+('Administração - Noturno', 2);
